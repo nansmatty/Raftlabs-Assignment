3e1a905cc2844a348b5f935e7886e4131a801968
Narayan Maity <narayanmaitysp1997@gmail.com>
2026-06-03 19:06:35 +0530

    format next.config.ts for consistency and clarity

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+	/* config options here */
+	reactCompiler: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'narayan-general-bucket.s3.ap-south-1.amazonaws.com',
+				port: '',
+				pathname: '/raftlabs-assignment/**',
+			},
+		],
+	},
 };
 
 export default nextConfig;
